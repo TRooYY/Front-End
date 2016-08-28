29b47655899a9702e63afb6db4fa5f6a3eb5f29e
TROY <troyking@126.com>
2016-08-28 21:21:51 +0800

    define an event

---
+++ event.js
@@ -0,0 +1,18 @@
+var Event = {
+	on: function(eventName, callback){
+		if(!this.handles){
+			this.handles = {};
+		}
+		if(! this.handles[eventName]){
+			this.handles[eventName] = [];
+		}
+		this.handles[eventName].push(callback);
+	},
+
+	emit: function(eventName){
+		if(this.handles[arguments[0]]){
+			for (var i = 0; i <= this.handles[arguments[0]].length; i++) {
+				this.handles[arguments[0]][i](arguments[1]);
+			}
+	}
+}
